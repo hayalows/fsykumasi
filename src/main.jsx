3c79376eec9e3f1f0df119cf8313fcee465eba1c
hayalows <mensahpkay+github@gmail.com>
2026-09-06 23:10:11 +0000

    Load final v12 sheet fix

--- src/main.jsx
+++ src/main.jsx
@@ -29,6 +29,7 @@ import "./account-page-v9.css";
 import "./operations-ux-v10.css";
 import "./access-assignments-v11.css";
 import "./access-assignments-v12.css";
+import "./access-assignments-v12-fix.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
