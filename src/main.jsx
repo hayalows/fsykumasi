df5de2da7c7b70bff8f041bb415d44c3ee7b085d
hayalows <mensahpkay+github@gmail.com>
2026-09-06 10:33:38 +0000

    Load registration modal refinements last

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ import "./housing-assignment-v4.css";
 import "./components/staff-role-transition.css";
 import "./modal-refinement-v2.css";
 import "./sidebar-navigation-v2.css";
+import "./registration-modal-v4.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
