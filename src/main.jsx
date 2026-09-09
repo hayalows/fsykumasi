f4be8b17da1799af157465a6b0d206dcf5d90b6b
hayalows <mensahpkay+github@gmail.com>
2026-09-09 09:18:21 +0000

    Load Wellness editor hotfix after release layers

--- src/main.jsx
+++ src/main.jsx
@@ -39,6 +39,7 @@ import "./ux-foundation-v30.css";
 import "./phase3-context-v31.css";
 import "./phase4-operations-v32.css";
 import "./phase5-release-v33.css";
+import "./pages/wellness-v35.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
