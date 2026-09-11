50b1bc95ef24bcc53c3d2c4bc4208fb7130671d8
hayalows <mensahpkay+github@gmail.com>
2026-09-11 17:06:46 +0000

    Use backward-compatible membership-aware arrival RPC

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -3,6 +3,7 @@ import { RegistrationJourneyV53 as RegistrationJourney } from "./RegistrationJou
 import { RegistrationReadinessV30 } from "./RegistrationReadinessV30.jsx";
 import { SessionFinalization } from "./SessionFinalization.jsx";
 import { PageHead, SegmentedControl } from "../components/UI.jsx";
+import { attemptParticipantMembershipCheckin } from "../lib/participant-membership.js";
 import "./registration-review.css";
 import "./registration-v5.css";
 import "./registration-journey.css";
@@ -60,6 +61,11 @@ export function Registration(props) {
     if (typeof window !== "undefined") window.location.reload();
   };
 
+  const membershipAwareCheckin = async (participantId, status) => {
+    if (!sessionId || status !== "arrived") return props.onCheckin?.(participantId, status);
+    return attemptParticipantMembershipCheckin({ sessionId, participantId });
+  };
+
   const cohortSummary = props.cohort;
   const modeMeta = MODE_META[mode];
   const journeyProps = {
@@ -69,7 +75,7 @@ export function Registration(props) {
     sessionId,
     capabilities,
     onOperationalDataChanged,
-    onCheckin: props.onCheckin,
+    onCheckin: membershipAwareCheckin,
     onUndoCheckin: props.onUndoCheckin,
     onSetOperationalStatus: props.onSetOperationalStatus,
   };
